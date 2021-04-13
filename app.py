a9012b613ab3391a301038539b7ad0c3495d4aa5
ayush-shah-1501 <aayushshah896@gmail.com>
2021-04-13 13:28:48 +0530

    commit

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import MySQLdb
 from flask import Flask, render_template, request, redirect
 
 
-project_dir = os.path.dirname(os.path.abspath(__file__))
-database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
+#project_dir = os.path.dirname(os.path.abspath(__file__))
+#database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:ayush1501@localhost/bookdatabase"
 
 db = SQLAlchemy(app)
 
